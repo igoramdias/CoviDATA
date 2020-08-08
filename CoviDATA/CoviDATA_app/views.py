bbcfcaeac117d7e2f70bdea3290ec02dccad89d7
igoramdias <igor.am.dias@gmail.com>
2020-08-08 02:58:27 -0300

    Conserto da index e funcionamento do modal

--- CoviDATA/CoviDATA_app/views.py
+++ CoviDATA/CoviDATA_app/views.py
@@ -8,7 +8,11 @@ def index(request):
     template = loader.get_template('index.html')
     context = { ## Relacionado com o banco de dados que sera usado para o template em questao
         'logo': illustrate.objects.get(name='logo'),
-        'brasil': illustrate.objects.get(name='brasil')
+        'brasil': illustrate.objects.get(name='brasil'),
+        'covid_1': illustrate.objects.get(name="covid_1"),
+        'covid_2': illustrate.objects.get(name="covid_2"),
+        'covid_3': illustrate.objects.get(name="covid_3"),
+        'lateral': illustrate.objects.get(name="lateral"),
     }
     return HttpResponse(template.render(context, request))
 
